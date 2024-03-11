b0e71c1d14d0f4ff49866e9326347ee58e5c611c
Paco Gimeno <PacoGim@outlook.com>
2024-03-11 18:53:40 +0100

    Added Œufs à la tomate et chorizo recipe

--- src/recipes/index.ts
+++ src/recipes/index.ts
@@ -1,6 +1,7 @@
 import mollejas_de_pollo_en_salsa_de_la_abuela from './mollejas_de_pollo_en_salsa_de_la_abuela.json'
 import poulet_moutarde from './poulet_moutarde.json'
+import oeufs_tomate_et_chorizo from './oeufs_tomate_et_chorizo.json'
 
-let recipes = [{ mollejas_de_pollo_en_salsa_de_la_abuela }, { poulet_moutarde }]
+let recipes = [{ mollejas_de_pollo_en_salsa_de_la_abuela }, { poulet_moutarde }, { oeufs_tomate_et_chorizo }]
 
 export default recipes
